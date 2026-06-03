5d9e4e80056b0fec8deaf73b80726aad1e7d65d8
Orysia Lebid <orysialebid@gmail.com>
2026-06-03 14:52:29 +0300

    Add mock news data in JSON format for testing purposes

--- Exam-2/Monticello/assets/js/dist/script.dev.js
+++ Exam-2/Monticello/assets/js/dist/script.dev.js
@@ -36,4 +36,101 @@ arrowBtn.addEventListener('click', function (event) {
     behavior: 'smooth',
     block: 'start'
   });
-});
+}); // news
+
+function loadNews() {
+  var response, newsData;
+  return regeneratorRuntime.async(function loadNews$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.prev = 0;
+          _context.next = 3;
+          return regeneratorRuntime.awrap(fetch('./mocks/news.json'));
+
+        case 3:
+          response = _context.sent;
+
+          if (response.ok) {
+            _context.next = 6;
+            break;
+          }
+
+          throw new Error('Не вдалося завантажити файл новин');
+
+        case 6:
+          _context.next = 8;
+          return regeneratorRuntime.awrap(response.json());
+
+        case 8:
+          newsData = _context.sent;
+          renderNews(newsData);
+          _context.next = 15;
+          break;
+
+        case 12:
+          _context.prev = 12;
+          _context.t0 = _context["catch"](0);
+          console.error('Помилка:', _context.t0);
+
+        case 15:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, null, null, [[0, 12]]);
+}
+
+function renderNews(news) {
+  var newsSlider = document.getElementById('lightSlider');
+  newsSlider.innerHTML = '';
+  news.forEach(function (item) {
+    var newsCard = "\n      <li>\n        <article class=\"news-card\">\n          <div class=\"news-card__image-wrapper\">\n            <img src=\"".concat(item.image, "\" alt=\"").concat(item.title, "\" class=\"news-card__image\">\n          </div>\n          \n          <div class=\"news-card__content\">\n            <a href=\"#\"><h3 class=\"news-card__title\">").concat(item.title, "</h3></a>\n            <p class=\"news-card__text\">").concat(item.summary, "</p>\n            \n            <div class=\"news-card__meta\">\n              <div class=\"news-card__author\">\n                <div class=\"news-card__avatar\">\n                  <img src=\"").concat(item.author.avatar, "\" alt=\"").concat(item.author.name, "\">\n                </div>\n                <h3 class=\"news-card__author-name\">").concat(item.author.name, "</h3>\n              </div>\n              <time class=\"news-card__date\" datetime=\"").concat(item.date, "\">").concat(item.date, "</time>\n            </div>\n          </div>\n        </article>\n      </li>\n    ");
+    newsSlider.insertAdjacentHTML('beforeend', newsCard);
+  });
+  initLightSlider();
+}
+
+function initLightSlider() {
+  var newsSlider = $("#lightSlider").lightSlider({
+    item: 3,
+    autoWidth: false,
+    slideMove: 1,
+    slideMargin: 20,
+    loop: true,
+    keyPress: true,
+    controls: false,
+    pager: true,
+    responsive: [{
+      breakpoint: 992,
+      settings: {
+        item: 2,
+        slideMove: 1
+      }
+    }, {
+      breakpoint: 576,
+      settings: {
+        item: 1,
+        slideMove: 1
+      }
+    }]
+  });
+  $('#goToPrevSlide').click(function () {
+    newsSlider.goToPrevSlide();
+  });
+  $('#goToNextSlide').click(function () {
+    newsSlider.goToNextSlide();
+  });
+}
+
+document.addEventListener('DOMContentLoaded', loadNews); // light gallery
+
+var galleryElement = document.getElementById('lightgallery');
+
+if (galleryElement) {
+  lightGallery(galleryElement, {
+    speed: 500,
+    licenseKey: '0000-0000-000-0000' // Додайте цей рядок
+
+  });
+}
--- Exam-2/Monticello/assets/js/script.js
+++ Exam-2/Monticello/assets/js/script.js
@@ -45,3 +45,104 @@ arrowBtn.addEventListener('click', (event) => {
     block: 'start'      
   });
 });
+
+// news
+
+async function loadNews() {
+  try {
+    const response = await fetch('./mocks/news.json'); 
+    
+    if (!response.ok) {
+      throw new Error('Не вдалося завантажити файл новин');
+    }
+    
+    const newsData = await response.json(); 
+    renderNews(newsData);                   
+    
+  } catch (error) {
+    console.error('Помилка:', error);
+  }
+}
+
+function renderNews(news) {
+  const newsSlider = document.getElementById('lightSlider');
+  newsSlider.innerHTML = '';
+  
+  news.forEach(item => {
+    const newsCard = `
+      <li>
+        <article class="news-card">
+          <div class="news-card__image-wrapper">
+            <img src="${item.image}" alt="${item.title}" class="news-card__image">
+          </div>
+          
+          <div class="news-card__content">
+            <a href="#"><h3 class="news-card__title">${item.title}</h3></a>
+            <p class="news-card__text">${item.summary}</p>
+            
+            <div class="news-card__meta">
+              <div class="news-card__author">
+                <div class="news-card__avatar">
+                  <img src="${item.author.avatar}" alt="${item.author.name}">
+                </div>
+                <h3 class="news-card__author-name">${item.author.name}</h3>
+              </div>
+              <time class="news-card__date" datetime="${item.date}">${item.date}</time>
+            </div>
+          </div>
+        </article>
+      </li>
+    `;
+    
+    newsSlider.insertAdjacentHTML('beforeend', newsCard);
+  });
+
+  initLightSlider();
+}
+
+function initLightSlider() {
+ const newsSlider = $("#lightSlider").lightSlider({
+    item: 3,                  
+    autoWidth: false,
+    slideMove: 1,             
+    slideMargin: 20,          
+    loop: true,               
+    keyPress: true,
+    controls: false,           
+    pager: true,              
+    responsive: [             
+      {
+        breakpoint: 992,
+        settings: {
+          item: 2,
+          slideMove: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          item: 1,
+          slideMove: 1
+        }
+      }
+    ]
+  });
+  $('#goToPrevSlide').click(function(){
+        newsSlider.goToPrevSlide(); 
+    });
+    $('#goToNextSlide').click(function(){
+       newsSlider.goToNextSlide(); 
+    });
+}
+
+document.addEventListener('DOMContentLoaded', loadNews);
+
+// light gallery
+
+const galleryElement = document.getElementById('lightgallery');
+if (galleryElement) {
+  lightGallery(galleryElement, {
+    speed: 500,
+    licenseKey: '0000-0000-000-0000' // Додайте цей рядок
+  });
+}
